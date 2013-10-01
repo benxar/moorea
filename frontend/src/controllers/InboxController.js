77e3afabd89359d4668ad69dc72cbe6288df3483
leonardoleenen <leonardo.leenen@agtech.com.ar>
2013-10-01 09:25:39 -0700

    se coloca angular a la tabla

--- frontend/src/controllers/InboxController.js
+++ frontend/src/controllers/InboxController.js
@@ -1,3 +1,9 @@
 function InboxController($scope, $http) {
-	
+	var oTable = $('.dataTable').dataTable({
+		 "sDom": "<'row-fluid tablePars'<'span10'f><'span2 right'l>r>t<'row-fluid'<'span3'i><'span9 right'p>>",
+			"sPaginationType": "bootstrap",
+			"oLanguage": {
+			"sLengthMenu": "Cantidad de _MENU_ "
+			}
+	});
 }
