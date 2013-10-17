7ac27f1627d47d26d4bbb60031e4e2b0eb5d623b
leonardoleenen <leonardo.leenen@agtech.com.ar>
2013-10-17 13:08:36 -0700

    se quita menú, footer y opciones del filtro avanzado

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -7,6 +7,10 @@ angular.module('app', []).
   $routes.
   
   	 
+      when('/inbox2', {
+          templateUrl: 'src/views/inbox2.html',
+          controller: Inbox2Controller
+          }).
       when('/inbox', {
           templateUrl: 'src/views/inbox.html',
           controller: InboxController
